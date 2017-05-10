cec093e1cf508c5e95cd3c9d261c0eb7f23546ab
Igor Vovk <ovodigr@gmail.com>
2017-05-10 19:47:35 +0300

    21

--- vk.js
+++ vk.js
@@ -9,22 +9,13 @@
 // ==/UserScript==
 
 var func = function(){
-   // console.log("1");  
    $("#ads_left").hide();
    $(".wall_marked_as_ads").parent().parent().parent().parent().parent().hide();
 };
 
-
 $(document).ready(function() 
 {
-
-	
    $("#ads_left").hide();
    $(".wall_marked_as_ads").parent().parent().parent().parent().parent().hide();
    setInterval(func,1000);
-
-	//setTimeout(function(){
-	//    $(".wall_marked_as_ads").parent().parent().parent().parent().parent().hide();
-	//},1000);
-
 }); 
